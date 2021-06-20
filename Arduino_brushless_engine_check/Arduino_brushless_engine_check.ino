75f252ab7fb704d3212a18569d0561edc25f6123
Максим Суганяк <mixer@MacBook-Air-Maksim.local>
2021-06-20 18:01:47 +0300

    Updated version for different engines

--- Arduino_brushless_engine_check/Arduino_brushless_engine_check.ino
+++ Arduino_brushless_engine_check/Arduino_brushless_engine_check.ino
@@ -15,17 +15,17 @@ void setup() {
   sixthESC.attach(7);
   firstESC.writeMicroseconds(2300);
   secondESC.writeMicroseconds(2300);
-  thirdESC.writeMicroseconds(2300);
-  fourthESC.writeMicroseconds(2300);  //настройка моторов(определение их минимальной и максимальной частоты вращения)
-  fifthESC.writeMicroseconds(2300);
-  sixthESC.writeMicroseconds(2300);
+  thirdESC.write(50);
+  fourthESC.write(50);  //настройка моторов(определение их минимальной и максимальной частоты вращения)
+  fifthESC.write(50);
+  sixthESC.write(50);
   delay(2000);
   firstESC.writeMicroseconds(800);
   secondESC.writeMicroseconds(800);
-  thirdESC.writeMicroseconds(800);
-  fourthESC.writeMicroseconds(800);
-  fifthESC.writeMicroseconds(800);
-  sixthESC.writeMicroseconds(800);
+  thirdESC.write(0);
+  fourthESC.write(0);
+  fifthESC.write(0);
+  sixthESC.write(0);
   delay(6000);
 
 
@@ -38,36 +38,36 @@ void setup() {
       delay(1000);
         secondESC.writeMicroseconds(0);
         
-    thirdESC.writeMicroseconds(1000);
+    thirdESC.write(50);
  delay(1000);
         thirdESC.writeMicroseconds(0);
         
-    fourthESC.writeMicroseconds(1000);
+    fourthESC.write(50);
       delay(1000);
         fourthESC.writeMicroseconds(0);
         
-    fifthESC.writeMicroseconds(1000);
+    fifthESC.write(50);
       delay(1000);
         fifthESC.writeMicroseconds(0);
         
-    sixthESC.writeMicroseconds(1000);
+    sixthESC.write(50);
       delay(1000);
         sixthESC.writeMicroseconds(0); // чек моторов по одному енд
     
     firstESC.writeMicroseconds(1000);
     secondESC.writeMicroseconds(1000);
-    thirdESC.writeMicroseconds(1000);
-    fourthESC.writeMicroseconds(1000); //чек верчения кручения всех моторов
-    fifthESC.writeMicroseconds(1000);
-    sixthESC.writeMicroseconds(1000);
+    thirdESC.write(50);
+    fourthESC.write(50); //чек верчения кручения всех моторов
+    fifthESC.write(50);
+    sixthESC.write(50);
     delay(4000);
 
     firstESC.writeMicroseconds(0);
     secondESC.writeMicroseconds(0);
-    thirdESC.writeMicroseconds(0);
-    fourthESC.writeMicroseconds(0); //остановка всех моторов
-    fifthESC.writeMicroseconds(0);
-    sixthESC.writeMicroseconds(0);
+    thirdESC.write(0);
+    fourthESC.write(0); //остановка всех моторов
+    fifthESC.write(0);
+    sixthESC.write(0);
     delay(4000);
   
  
@@ -75,11 +75,12 @@ void setup() {
  
 void loop() {
  int value = map(analogRead(0), 0, 1023, 800, 2300); //инициализация крутилки
+ int value2 = map(analogRead(0), 0, 1023, 0, 60);
 
     firstESC.writeMicroseconds(value);
     secondESC.writeMicroseconds(value);
-    thirdESC.writeMicroseconds(value);
-    fourthESC.writeMicroseconds(value);
-    fifthESC.writeMicroseconds(value);  // верчение всех моторов управлением с крутилки
-    sixthESC.writeMicroseconds(value);
+    thirdESC.write(value2);
+    fourthESC.write(value2);
+    fifthESC.write(value2);  // верчение всех моторов управлением с крутилки
+    sixthESC.write(value2);
 }
